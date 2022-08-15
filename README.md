218f68eadc33fd1bf1efb0dee9b1075d3687b3c3
ErrorNoInternet <error.nointernet@gmail.com>
2022-08-15 14:54:17 +0800

    Final fixes

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ mkfs.ext2 -help
 mkfs.ext2 -device file.ext2
 
 # Create filesystem on real device (200 MB)
-mkfs.ext2 -device /dev/sdX -blocks 5120
+mkfs.ext2 -device /dev/sdX -blocks 51200
 
 # Create filesystem on real device (400 MB)
 mkfs.ext2 -device /dev/sdX -blocks 102400
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 
 	"github.com/ErrorNoInternet/mkfs.ext2/filesystem"
 )
@@ -12,11 +13,20 @@ func main() {
 	var blockSize, blocks int
 	flag.StringVar(&devicePath, "device", "", "The device you want to create a filesystem on")
 	flag.IntVar(&blockSize, "blockSize", 4096, "The block size of each block in the filesystem")
-	flag.IntVar(&blocks, "blocks", 2560, "The amount of blocks to create in the filesystem")
+	flag.IntVar(&blocks, "blocks", 0, "The amount of blocks to create in the filesystem")
 	flag.Parse()
 	if devicePath == "" {
 		flag.Usage()
 		return
+	}
+
+	if blocks == 0 {
+		deviceObject, err := os.Stat(devicePath)
+		if err != nil {
+			blocks = 25600
+		} else {
+			blocks = int(deviceObject.Size() / int64(blockSize))
+		}
 	}
 
 	err := filesystem.Make(devicePath, blockSize, blocks)
